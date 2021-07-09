a7f62bceabf1c1650bce92dff34421a9b0966148
Marcela Torres <itsmarcy@gmail.com>
2021-07-09 13:57:56 -0500

    Add tests

--- tests/sample_tracker/test_sample_tracker.py
+++ tests/sample_tracker/test_sample_tracker.py
@@ -1,7 +1,7 @@
 import pytest
 
 from gift_wrap.sample_tracker import SampleTracker
-import logging
+from gift_wrap.sample_tracker.exceptions import SampleTrackerFailedUpload
 
 @pytest.fixture
 def sample_tracker_obj():
@@ -9,8 +9,7 @@ def sample_tracker_obj():
     yield SampleTracker()
 
 
-def test_sampletracker_post_success(sample_tracker_obj, caplog):
-    caplog.set_level(logging.DEBUG)
+def test_sampletracker_post_success(sample_tracker_obj):
     record = {
         "samplename": "fakesample",
         "statekey": "FAKE_STATE",
@@ -19,3 +18,21 @@ def test_sampletracker_post_success(sample_tracker_obj, caplog):
     assert response == "Upload success"
 
 
+def test_sampletracker_post_missingkey_samplename(sample_tracker_obj):
+    """ Test that an exception is thrown if a samplename is missing """
+    record = {
+        "samplename": "fakesample",
+    }
+    with pytest.raises(SampleTrackerFailedUpload) as excinfo:
+        sample_tracker_obj.post(record)
+        assert "missing statekey" in excinfo.value
+
+
+def test_sampletracker_post_missingkey_statekey(sample_tracker_obj):
+    """ Test that an exception is thrown if a statekey is missing """
+    record = {
+        "statekey": "fakestate",
+    }
+    with pytest.raises(SampleTrackerFailedUpload) as excinfo:
+        sample_tracker_obj.post(record)
+        assert "missing samplename" in excinfo.value
