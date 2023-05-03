24c76402b8a121b5370602e06ec6d85723358e80
Marcela Torres <itsmarcy@gmail.com>
2023-05-02 20:20:29 -0500

    Update rclone to be more generic

--- gift_wrap/rclone/rclone.py
+++ gift_wrap/rclone/rclone.py
@@ -1,55 +1,60 @@
+import subprocess
+import logging
 from pathlib import Path
 from subprocess import CompletedProcess
+from typing import Optional
 
-from gift_wrap.utils.utils import load_json_file, subprocess_cmd
+from gift_wrap.utils.utils import subprocess_cmd
+from . import exceptions
 
-
-def copy_to(source: str, dest: str, rclone_config: str, *args) -> CompletedProcess:
-    """Uses rclone to copy file from GCP to s3"""
-    extra_args = [f"--{arg}" for arg in args]
-    command = [
-        "rclone",
-        "--config",
-        rclone_config,
-        "copyto",
-        source,
-        dest,
-        "--checksum",
-        "--error-on-no-transfer",
-        "-vv",
-        *extra_args,
-    ]
-    return subprocess_cmd(command)
+logger = logging.getLogger(__name__)
 
 
-def convert_cloud_path(path: str) -> str:
-    """Converts the cloud path to the format that rclone prefers"""
-    bucket_name = path.split("/")[2]
-    cloud_service = path.split("/")[0]
-    key = "/".join(path.split("/")[3:])
-    return f"{cloud_service}{bucket_name}/{key}"
+class RClone:
+    """A wrapper around the rlcone class."""
+
+    def __init__(self, config_path: Optional[str] = None) -> None:
+        self.config_path = config_path
+        self._required_args = ["--checksum", "--error-on-no-transfer", "-vv"]
+
+    def copy_to(self, source: str, dest: str, *args) -> CompletedProcess:
+        """Uses rclone to copy file from GCP to s3"""
+        command = self._initialize_command()
+        command.extend(["copyto", source, dest, *self._required_args, *args])
+        return self._submit_command(command)
+
+    def copy_url(self, source: str, dest: str, *args) -> CompletedProcess:
+        """Uses rclone to copy the contents of url to the given destination"""
+        command = self._initialize_command()
+        command.extend(["copyurl", source, dest, *self._required_args, *args])
+        return self._submit_command(command)
+
+    def _initialize_command(self) -> list[str]:
+        """The first set of commands to occur before the user submitted ones."""
+        command = ["rclone"]
+        if self.config_path:
+            command.extend(["--config", self.config_path])
+        return command
+
+    def _submit_command(self, command: list[str]) -> CompletedProcess:
+        """Submits the rclone command with subprocess"""
+        try:
+            return subprocess_cmd(command)
+        except subprocess.CalledProcessError as err:
+            if "Unchanged skipping" in err.stderr:
+                raise exceptions.FileAlreadyExist from err
+            if "There was nothing to transfer" in err.stderr:
+                raise exceptions.FileDoesNotExist from err
+            raise exceptions.RCloneError(err.stderr or err.stdout) from err
 
 
-def create_rclone_config(
-    credentials_path: str, working_dir: Path, region: str = "us-east-1"
-) -> str:
-    """Creates the config for rclone"""
-    gcp_project_id = get_gcp_project_id(credentials_path)
+def create_generic_config(working_dir: Path) -> str:
+    """Creates a generic config for rclone. The default settings are to use
+    GCP and AW2 environment variables."""
     config_path = working_dir / "intake-gvcf-tmp-rclone.conf"
     with open(config_path, "w", encoding="utf-8") as fout:
-        fout.write(
-            RCLONE_CONFIG_TEMPLATE.format(
-                GCP_PROJECT_ID=gcp_project_id,
-                GCP_CRED_PATH=credentials_path,
-                REGION=region,
-            )
-        )
+        fout.write(RCLONE_CONFIG_TEMPLATE)
     return str(config_path)
-
-
-def get_gcp_project_id(gcp_credentials_path: str) -> str:
-    """Returns the project id of the gcp service account used"""
-    return load_json_file(gcp_credentials_path)["project_id"]
 
 
 #############
@@ -62,10 +67,11 @@ type = s3
 provider = AWS
 env_auth = true
 no_check_bucket = true
-region = {REGION}
 
 [gs]
 type = google cloud storage
-project_number = {GCP_PROJECT_ID}
-service_account_file = {GCP_CRED_PATH}
+env_auth = true
+
+[local]
+
 """
